4012edb0c12d225b01c700d3ae54ae059268079d
DamascenoV <victor.c.damasceno@gmail.com>
2024-01-06 21:29:53 +0000

    Add Skim

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate skim;
+use skim::prelude::{*};
+use std::io::Cursor;
+
 use clap::{Parser, Subcommand};
 
 mod database;
@@ -11,13 +15,44 @@ struct Urls {
     command: Option<Commands>,
 }
 
+impl Urls {
+    fn select_command(&self) {
+        match &self.command {
+            Some(Commands::Get { name }) => {
+                //println!("Get: {:?}", database::get_by_name(name.to_string()));
+                println!("Get")
+            }
+            Some(Commands::Insert { name, url }) => {
+                //println!("Insert: {:?}", database::insert(&models::Url::new(0, name.to_string(), url.to_string())));
+                println!("Insert")
+            }
+            Some(Commands::Update { name, url }) => {
+                println!("Update")
+            }
+            Some(Commands::List) => {
+                println!("List")
+            }
+            Some(Commands::Delete { name }) => {
+                println!("Delete")
+            }
+            None => list(),
+        }
+    }
+}
+
 #[derive(Subcommand)]
 enum Commands {
     Get { name: String },
     Insert { name: String, url: String },
     Update { name: String, url: String },
-    List ,
+    List,
     Delete { name: String },
+}
+
+impl Commands {
+    fn get_all_commands() -> String {
+        return "get\ninsert\nupdate\ndelete".to_string();
+    }
 }
 
 fn main() {
@@ -29,25 +64,27 @@ fn main() {
         Err(err) => println!("Error creating database, {}", err),
     }
 
-    match &ur_url.command {
-        Some(Commands::Get { name }) => {
-            //println!("Get: {:?}", database::get_by_name(name.to_string()));
-            println!("Get")
-        },
-        Some(Commands::Insert { name, url }) => {
-            //println!("Insert: {:?}", database::insert(&models::Url::new(0, name.to_string(), url.to_string())));
-            println!("Insert")
-        }
-        Some(Commands::Update { name, url }) => {
-            println!("Update")
-        },
-        Some(Commands::List) => {
-            println!("List")
-        },
-        Some(Commands::Delete { name }) => {
-            println!("Delete")
-        },
-        None => println!("No command, Go to list Commands"),
-    }
+    ur_url.select_command();
+}
 
+fn list() {
+    let commands = Commands::get_all_commands();
+
+    let options = SkimOptionsBuilder::default()
+        .height(Some("100%"))
+        .multi(true)
+        .reverse(true)
+        .build()
+        .unwrap();
+
+    let item_reader = SkimItemReader::default();
+    let items = item_reader.of_bufread(Cursor::new(commands));
+
+    let selected = Skim::run_with(&options, Some(items))
+        .map(|out| out.selected_items)
+        .unwrap_or_else(|| vec![]);
+
+    for item in selected.iter() {
+        print!("{}{}", item.output(), "\n");
+    }
 }
